387d5fcc26ecbfa5351f4b0fec53787f2bf94e26
Sebastian Raaphorst <srcoding@gmail.com>
2019-05-25 03:18:50 -0400

    Cleaning up comments.

--- dcp_jvm/src/main/scala/dcp/day048/BinaryTree.scala
+++ dcp_jvm/src/main/scala/dcp/day048/BinaryTree.scala
@@ -18,21 +18,10 @@ case class BinaryTree[T](value: T, left: Option[BinaryTree[T]], right: Option[Bi
 
 object BinaryTree {
   def apply[T](preorder: List[T], inorder: List[T]): Option[BinaryTree[T]] = {
-    // If postorder head is current node, then it is None to the left and pop it.
-    // If postorder head is current node and left is none, then we go to right.
-    // If both lists are done, then we are done, so this case should never come up.
-
-    // Always create a node if preorder is nonempty.
-
-    // If postorder head is preorder head, left is None.
-    // Otherwise it is BinaryTree(preorder.tail, postorder)
-
-    // drop while not node in postorder.
-    // Then that is right.
     preorder.headOption.map { value =>
       // Divide into left preOrder, right preOrder, left inOrgder, right inOrder.
-      // a gives 3, splitAt gives bde, cfg which is what we want.
-      // Now we need the inorder. We want dbe and fcg
+      // In example, a gives 3, splitAt gives bde, cfg which is what we want.
+      // Now we need the inorder. We want dbe and fcg.
       val inOrderIdx = inorder.indexOf(value)
       val (leftPreorder, rightPreorder) = preorder.tail.splitAt(inOrderIdx)
       val leftInorder = inorder.take(inOrderIdx)
