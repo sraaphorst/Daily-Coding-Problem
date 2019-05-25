2b09582cf2335b78762d993d6aeb8d112330f74c
Sebastian Raaphorst <srcoding@gmail.com>
2019-05-25 03:17:41 -0400

    Prettifying day 48 tests.

--- dcp_jvm/src/main/scala/dcp/day048/BinaryTree.scala
+++ dcp_jvm/src/main/scala/dcp/day048/BinaryTree.scala
@@ -38,6 +38,8 @@ object BinaryTree {
       val leftInorder = inorder.take(inOrderIdx)
       // We need a slice from inOrderIdx + 1 to the end:
       val rightInorder = inorder.takeRight(inorder.length - inOrderIdx - 1)
+
+//      Debugging:
 //      println(s"VALUE=$value")
 //      println(s"\tpreorder=$preorder")
 //      println(s"\tinorder=$inorder")
@@ -49,48 +51,47 @@ object BinaryTree {
 
       BinaryTree(value, apply(leftPreorder, leftInorder), apply(rightPreorder, rightInorder))
     }
-
-//    preorder.headOption.filter { inorder.contains}.map {value =>
-//      // Divide into left preOrder, right preOrder, left inOrgder, right inOrder.
-//      // a gives 3, splitAt gives bde, cfg which is what we want.
-//      // Now we need the inorder. We want dbe and fcg
-//      val inOrderIdx = inorder.indexOf(value)
-//      val (leftPreorder, rightPreorder) = preorder.tail.splitAt(inOrderIdx)
-//      val leftInorder = inorder.take(inOrderIdx)
-//      val rightInorder = inorder.takeRight(inOrderIdx)
-//      println(s"VALUE=$value")
-//      println(s"\tidx=$inOrderIdx")
-//      println(s"\tLeft Preorder=$leftPreorder")
-//      println(s"\tRight Preorder=$rightPreorder")
-//      println(s"\tLeft Inorder=$leftInorder")
-//      println(s"\tRight Inorder=$rightInorder")
-//
-//      BinaryTree(value, apply(leftPreorder, leftInorder), apply(rightPreorder, rightInorder))
-//    }
   }
 }
 
+// A random sampling of test cases. Specification testing is also done in TestDay048.
 object Test extends App {
-  val preorder  = List("a", "b", "d", "e", "c", "f", "g")
-  val inorder = List("d", "b", "e", "a", "f", "c", "g")
-  val postorder = List("d", "e", "b", "f", "g", "c", "a")
+  // Tree 1: Given with problem
+  val preorder1  = List("a", "b", "d", "e", "c", "f", "g")
+  val inorder1 = List("d", "b", "e", "a", "f", "c", "g")
+  val postorder1 = List("d", "e", "b", "f", "g", "c", "a")
 
-  val tree = BinaryTree("a",
-    Some(BinaryTree("b", Some(BinaryTree("d", None, None)), Some(BinaryTree("e", None, None)))),
-    Some(BinaryTree("c", Some(BinaryTree("f", None, None)), Some(BinaryTree("g", None, None)))))
+  val tree1 = {
+    val g = BinaryTree("g", None, None)
+    val f = BinaryTree("f", None, None)
+    val c = BinaryTree("c", Some(f), Some(g))
+    val e = BinaryTree("e", None, None)
+    val d = BinaryTree("d", None, None)
+    val b = BinaryTree("b", Some(d), Some(e))
+    val a = BinaryTree("a", Some(b), Some(c))
+    a
+  }
+  assert(preorder1  == tree1.preorder)
+  assert(inorder1   == tree1.inorder)
+  assert(postorder1 == tree1.postorder)
+  assert(BinaryTree(preorder1, inorder1).contains(tree1))
 
-  assert(preorder == tree.preorder)
-  assert(inorder  == tree.inorder)
-  assert(postorder == tree.postorder)
-  assert(BinaryTree(preorder, inorder).contains(tree))
-
+  // A random smattering of problems that were used to debug an issue in the original implementation.
   val preorder2 = List(1, 2, 3)
   val inorder2  = List(2, 3, 1)
-  val tree2 = BinaryTree(1, Some(BinaryTree(2, None, Some(BinaryTree(3, None, None)))), None)
+
+  val tree2 = {
+    val n3 = BinaryTree(3, None, None)
+    val n2 = BinaryTree(2, None, Some(n3))
+    val n1 = BinaryTree(1, Some(n2), None)
+    n1
+  }
   assert(tree2.preorder == preorder2)
   assert(tree2.inorder == inorder2)
   assert(BinaryTree(preorder2, inorder2).contains(tree2))
 
+  val preorder3 = List(1, 2, 3, 4)
+  val inorder3  = List(2, 1, 4, 3)
   val tree3 = {
     val n4 = BinaryTree(4, None, None)
     val n3 = BinaryTree(3, Some(n4), None)
@@ -98,23 +99,26 @@ object Test extends App {
     val n1 = BinaryTree(1, Some(n2), Some(n3))
     n1
   }
-  val preorder3 = List(1, 2, 3, 4)
-  val inorder3  = List(2, 1, 4, 3)
   assert(tree3.preorder == preorder3)
   assert(tree3.inorder  == inorder3)
   assert(BinaryTree(preorder3, inorder3).contains(tree3))
 
+  val preorder4 = List(1, 2)
+  val inorder4  = List(1, 2)
   val tree4 = {
     val n2 = BinaryTree(2, None, None)
     val n1 = BinaryTree(1, None, Some(n2))
     n1
   }
-  val preorder4 = List(1, 2)
-  val inorder4  = List(1, 2)
   assert(BinaryTree(preorder4, inorder4).contains(tree4))
 
-  val tree5 = BinaryTree(1, Some(BinaryTree(2, None, None)), Some(BinaryTree(3, None, None)))
   val preorder5 = List(1, 2, 3)
   val inorder5  = List(2, 1, 3)
+  val tree5 = {
+    val n3 = BinaryTree(3, None, None)
+    val n2 = BinaryTree(2, None, None)
+    val n1 = BinaryTree(1, Some(n2), Some(n3))
+    n1
+  }
   assert(BinaryTree(preorder5, inorder5).contains(tree5))
 }
