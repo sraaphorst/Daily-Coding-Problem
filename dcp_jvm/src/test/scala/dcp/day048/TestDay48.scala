d1ddb19946dc30e0716a3643aba900a2056fb172
Sebastian Raaphorst <srcoding@gmail.com>
2019-05-25 03:00:26 -0400

    Day 48 test cleanup.

--- dcp_jvm/src/test/scala/dcp/day048/TestDay48.scala
+++ dcp_jvm/src/test/scala/dcp/day048/TestDay48.scala
@@ -21,19 +21,19 @@ class TestDay48 extends FlatSpec with Matchers with GeneratorDrivenPropertyCheck
     * We do a bit of hackery to try to limit the cases where the numbers will not be unique, but since the calls
     * to generate subtrees are independent, used is only so useful, so we need to add an implication to the spec.
     */
-  def genTree[T: Arbitrary](used: Set[T])(maxDepth: Int): Gen[BinaryTree[T]] =
+  def genTree[T: Arbitrary](maxDepth: Int, used: Set[T] = Set.empty[T]): Gen[BinaryTree[T]] =
     if (maxDepth <= 0) {
       for (value <- arbitrary[T]) yield BinaryTree[T](value, None, None)
     } else for {
       value <- arbitrary[T]
-      l <- Gen.lzy(Gen.option(genTree[T](used + value)(maxDepth-1)))
-      r <- Gen.lzy(Gen.option(genTree[T](used + value)(maxDepth-1)))
+      l <- Gen.lzy(Gen.option(genTree[T](maxDepth-1, used + value)))
+      r <- Gen.lzy(Gen.option(genTree[T](maxDepth-1, used + value)))
     } yield {
       BinaryTree[T](value, l, r)
     }
 
   "The binary tree construction" should "decompose and recompose" in {
-    forAll (genTree[Int](Set.empty)(20)) { bt =>
+    forAll (genTree[Int](20)) { bt =>
       (bt.size != bt.preorder.size) ==>
         BinaryTree(bt.preorder, bt.inorder).contains(bt)
     }
