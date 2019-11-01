e59b28323b6c0f7c89f6283ed6e187768ae243c2
Sebastian Raaphorst <sraaphorst@gmail.com>
2019-11-01 19:42:59 -0300

    Added day 204.

--- dcp_kotlin/src/main/kotlin/dcp/day204/day204.kt
+++ dcp_kotlin/src/main/kotlin/dcp/day204/day204.kt
@@ -2,56 +2,23 @@ package dcp.day204
 // dcp.day204.kt
 // By Sebastian Raaphorst, 2019.
 
-data class CBT<T>(val value: T, val left: CBT<T>? = null, val right: CBT<T>? = null) {
-    // We need a function that takes a CBT<T>? and moves to a CBT<T>?
-    private fun aux(depth: Int, mover: () -> CBT<T>?): Int {
-        val next = mover()
-        return if (next != null) {
-            aux(depth + 1, mover)
-        } else {
-            depth
-        }
-    }
+data class CBT<T>(val value: T, val left: CBT<T>? = null, val right: CBT<T>? = null)
 
-    fun leftDepth(): Int {
-        return aux(1) { this.left }
-    }
-
-    fun rightDepth(): Int {
-        return aux(1) { this.right }
-    }
-}
-
-typealias T = CBT<Int>
-
-
-fun <T> findLefHeight(tree: CBT<T>?): Int {
+private fun <T> findHeight(tree: CBT<T>?, mover: (CBT<T>?) -> CBT<T>?): Int {
     var height = 0
     var current = tree
-    while (current != null) {
-        current = current.left
+    while (mover(current) != null) {
+        current = mover(current)
         height += 1
     }
     return height
 }
 
-fun <T> findRightHeight(tree: CBT<T>?): Int {
-    var height = 0
-    var current = tree
-    while (current != null) {
-        current = current.right
-        height += 1
-    }
-    return height
-}
+fun <T> countNodes(current: CBT<T>?): Int {
+    if (current == null)
+        return 0
 
-// We need to find a way to traverse the bottom row from either the right or the left.
-// The right is preferable because that is where the empty nodes will be so we can detect them
-// without traversing the row.
-fun <T> countNodes(tree: CBT<T>?): Int {
-    var current: CBT<T>? = tree
-
-    val leftHeight = findLefHeight(current)
-    val rightHeight = findRightHeight(tree)
-    return if (leftHeight == rightHeight) (2 shl leftHeight - 1) else countNodes(tree?.left) + countNodes(tree?.right) + 1
+    val leftHeight = findHeight(current) { it?.left }
+    val rightHeight = findHeight(current) { it?.right }
+    return if (leftHeight == rightHeight) (2 shl leftHeight) - 1 else countNodes(current.left) + countNodes(current.right) + 1
 }
--- dcp_kotlin/src/test/kotlin/dcp/day204/TreeTester.kt
+++ dcp_kotlin/src/test/kotlin/dcp/day204/TreeTester.kt
@@ -5,14 +5,14 @@ import kotlin.test.assertEquals
 
 class TreeTester {
     @Test
-    fun fullTreeTest() {
-        val t = CBT(1,
-            CBT(2,
-                CBT(3,
-                    CBT(4), CBT(5)),
-                CBT(6, CBT(7))))
-        println(t)
-        //assertEquals(countNodes(t), 7)
-        assertEquals(true, true)
+    fun treeTest() {
+        val t = CBT(1, CBT(2, CBT(4), CBT(5)), CBT(3, CBT(6), CBT(7)))
+        assertEquals(countNodes(t), 7)
+
+        val t2 = CBT(1, CBT(2, CBT(4), CBT(5)), CBT(3, CBT(6)))
+        assertEquals(countNodes(t2), 6)
+
+        val t3 = CBT(1, CBT(2, CBT(4), CBT(5)), CBT(3))
+        assertEquals(countNodes(t3), 5)
     }
 }
