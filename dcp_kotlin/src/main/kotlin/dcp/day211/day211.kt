c3070f6324d6858bda2d7b28c925570caec93b98
sraaphorst <sraaphorst@gemini.edu>
2019-11-05 12:01:30 -0300

    Added rolling hash to 211.

--- dcp_kotlin/src/main/kotlin/dcp/day211/day211.kt
+++ dcp_kotlin/src/main/kotlin/dcp/day211/day211.kt
@@ -1,4 +1,9 @@
 package dcp.day211
+// day211.kt
+// By Sebastian Raaphorst, 2019.
+
+
+fun Char.hash(): Int = this.toInt() - 'a'.toInt() + 1
 
 // Scan windows for substring.
 fun findStartPoints(str: String, substr: String): Set<Int> {
@@ -11,40 +16,9 @@ fun findStartPoints(str: String, substr: String): Set<Int> {
     }.toSet()
 }
 
-// Use a simple rolling hash to scan the windows for pattern.
-// This uses fewer operations: for each window, we simply add a value and then subtract a value instead of looping
-// over len(substr) items.
-//fun findStartPointsRollingHash(str: String, substr: String): Set<Int> {
-//    // Convert a character into an int.
-//    fun aux(s: Char): Int = s.toInt() - 'A'.toInt() + 1
-//
-//    val strLen = str.length
-//    val substrLen = substr.length
-//
-//    // The hash of the substring.
-//    val subStrHash = substr.map { aux(it) }.sum()
-//
-//    // The current position in the string and the current hash value.
-//    var hash = 0
-//    for (pos in 0..strLen) {
-//        if (pos > substrLen)
-//        // Remove the first character, if applicable.
-//            hash -= aux(substr[pos - substrLen])
-//        // Add the next character.
-//        hash += aux(str[pos])
-//
-//        // Check the hash.
-//        if (hash == subStrHash) {
-//            // If they match, do a more comprehensive scan.
-//        }
-//    }
-//
-//}
-
 fun findStartPointsRollingHash(str: String, substr: String): Set<Int> {
     require(str.length >= substr.length) { "Main string must be longer than substring" }
     // Convert a character into an int.
-    fun Char.hash(): Int = this.toInt() - 'a'.toInt() + 1
 
     val strLen = str.length
     val substrLen = substr.length
@@ -56,25 +30,11 @@ fun findStartPointsRollingHash(str: String, substr: String): Set<Int> {
     val startHash = str.take(substrLen).map { it.hash() }.sum()
 
     fun aux(pos: Int, hash: Int): Set<Int> {
-        // Check to see if the requirements are met.
-        println("Checking hash values: substr=$substrHash, current=$hash")
-        val set: Set<Int> = {
-            if (hash == substrHash) {
-                // Check to see if the strings are actually equal.
-                val hashStr = str.subSequence(pos, pos + substrLen)
-                println("\tCheck to see if strings are equal: $substr, $hashStr")
-                if (substr == hashStr) {
-                    println("\t\t*** MATCH: pos=$pos")
-                    setOf(pos)
-                } else emptySet()
-            } else emptySet()
-        }()
+        // Check to see if the requirements are met for this to quality as a possible candidate.
+        val set = if (hash == substrHash && str.subSequence(pos, pos + substrLen) == substr) setOf (pos) else emptySet()
 
-        // Update the hash if possible.
-        println("Trying to update the hash with ${hash + 1}. Max val is ${strLen - substrLen}.")
+        // Update the hash code if possible to get the new set of values.
         val newset = if (pos + substrLen < strLen) {
-            println("Removing the bottom position ${str[pos]} = ${str[pos].hash()}.")
-            println("Adding top position ${str[pos + substrLen]} = ${str[pos + substrLen].hash()}")
             val newHash = hash - str[pos].hash() + str[pos + substrLen].hash()
             aux(pos + 1, newHash)
         } else {
@@ -89,4 +49,5 @@ fun findStartPointsRollingHash(str: String, substr: String): Set<Int> {
 fun main() {
     println(findStartPointsRollingHash("abcdbcebccbc", "bc"))
     println(findStartPointsRollingHash("abracadabra", "abr"))
+    println(findStartPointsRollingHash("essentialial", "ial"))
 }
