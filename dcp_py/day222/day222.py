c31e3ac7f97a3414a0f779e333b013473a6351c0
sraaphorst <sraaphorst@gemini.edu>
2019-11-14 15:03:41 -0300

    Finished day 222.

--- dcp_py/day222/day222.py
+++ dcp_py/day222/day222.py
@@ -9,18 +9,32 @@ def simplify_pathname(path_name: str) -> str:
     :param path_name: the pathname, which may contain . or ..
     :return: the simplified pathname
 
-    >>> simplify_pathname('/usr/bin/../bin/./scripts/../')
-    '/usr/bin/'
     >>> simplify_pathname('/')
     '/'
+    >>> simplify_pathname('//////usr/bin/')
+    '/usr/bin/'
+    >>> simplify_pathname('/../../../../usr/bin')
+    '/usr/bin'
+    >>> simplify_pathname('/usr/bin/../bin/./scripts/../')
+    '/usr/bin/'
+    >>> simplify_pathname('/home/')
+    '/home/'
     """
+    # Collapse the /////... strings since they do nothing.
+    while '//' in path_name:
+        path_name = path_name.replace('//', '/')
+
     pieces = path_name.split('/')
     stack = []
 
     for piece in pieces:
         if piece == '..':
-            stack.pop()
+            if len(stack) > 0:
+                stack.pop()
         elif piece != '.':
             stack.append(piece)
 
+    # We need a leading '' to get the initial /.
+    if stack[0] != '':
+        stack.insert(0, '')
     return '/'.join(stack)
