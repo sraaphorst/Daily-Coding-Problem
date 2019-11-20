ee300e4576a5b7240d475dfbd3e4fa8ee85dd341
Sebastian Raaphorst <sraaphorst@gmail.com>
2019-11-20 00:54:13 -0300

    Added two more test cases.

--- dcp_kotlin/src/test/kotlin/dcp/day226/AlphabetSorter.kt
+++ dcp_kotlin/src/test/kotlin/dcp/day226/AlphabetSorter.kt
@@ -5,8 +5,20 @@ import kotlin.test.assertEquals
 
 class AlphabetSorter {
     @Test
-    fun alphabetSorted() {
+    fun alphabetSorted1() {
         val words = listOf("xww", "wxyz", "wxyw", "ywx", "ywz")
         assertEquals(findAlphabet(words), listOf('x', 'z', 'w', 'y'))
+    }
+
+    @Test
+    fun alphabetSorted2() {
+        val words = listOf("caa", "aaa", "aab")
+        assertEquals(findAlphabet(words), listOf('c', 'a', 'b'))
+    }
+
+    @Test
+    fun alphabetSorted3() {
+        val words = listOf("wrt", "wrf", "er", "ett", "rftt")
+        assertEquals(findAlphabet(words), listOf('w', 'e', 'r', 't', 'f'))
     }
 }
