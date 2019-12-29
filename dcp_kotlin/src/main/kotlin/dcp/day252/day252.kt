d1d14642c77fa0e8d324c22a972675ae77bbbf97
Sebastian Raaphorst <sraaphorst@gmail.com>
2019-12-29 17:57:30 -0300

    Fixed day 252.

--- dcp_kotlin/src/main/kotlin/dcp/day252/day252.kt
+++ dcp_kotlin/src/main/kotlin/dcp/day252/day252.kt
@@ -3,6 +3,7 @@ package dcp.day252
 // By Sebastian Raaphorst, 2019.
 
 import java.math.BigInteger
+import kotlin.math.ceil
 
 
 /**
@@ -16,24 +17,22 @@ var ratsGE2 = sequence {
     }
 }
 
-// Cannot seem to make this tailrec.
+
 fun Rational.toEgyptianRational(): List<Rational> {
+    tailrec
     fun aux(r: Rational, iter: Iterator<Rational>, list: List<Rational>): List<Rational>? {
         // If we have reached 0, we are at the end and have our answer.
-        if (r.equals(Rational.ZERO))
+        if (r == Rational.ZERO)
             return list
 
         // If the remaining numerator is zero, then we have found the solution and can shortcut.
-        if (r.numerator.equals(BigInteger.ONE))
-            return list + r
-
-        val n = iter.next()
+        val n = if (r.numerator == BigInteger.ONE) r else iter.next()
 
         // Check if this fits.
-        if (r - n >= Rational.ZERO)
-           return aux(r - n, iter, list + n)
+        return if (r - n >= Rational.ZERO)
+            aux(r - n, iter, list + n)
         else
-            return aux(r, iter, list)
+            aux(r, iter, list)
     }
 
     return aux(this, ratsGE2.iterator(), emptyList())!!
@@ -59,4 +58,21 @@ fun Rational.toEgyptianRationalImp(): List<Rational> {
     }
 
     return list
+}
+
+// A more direct way of jumping to the next denominator, but which may fail due to floating point arithmetic (e.g. 493/496)
+fun Rational.toEgyptianRationalDirect(): List<Rational>? {
+    if (numerator == BigInteger.ZERO) return emptyList()
+    val list = mutableListOf<Rational>()
+    var curr = this
+    while (curr.numerator != BigInteger.ONE) {
+        val next = Rational(BigInteger.ONE, ceil(curr.denominator.toDouble() / curr.numerator.toDouble()).toInt().toBigInteger())
+        list.add(next)
+        curr -= next
+
+        // If this happens, it's because of floating point arithmetic imprecision.
+        if (curr < Rational.ZERO)
+            return null
+    }
+    return list + curr
 }
--- dcp_kotlin/src/test/kotlin/dcp/day252/day252.kt
+++ dcp_kotlin/src/test/kotlin/dcp/day252/day252.kt
@@ -16,19 +16,18 @@ class RationalGen: Gen<Rational> {
     }
 
     override fun random(): Sequence<Rational> = generateSequence {
-        val n = Gen.choose(0, 1_000).random().first()
-        val d = Gen.choose(n+1, 1_000).random().first()
+        val n = Gen.choose(1, 100).random().first()
+        val d = Gen.choose(n+1, 101).random().first()
         n divBy d
     }
 }
 
-
 class EgyptianRationalRecursiveTest: StringSpec() {
     init {
         "recursive attempt" {
-            forAll(10, RationalGen()) { r ->
-                val egyptian = try {r.toEgyptianRational()} catch (_: StackOverflowError) { null }
-                val sum = egyptian?.fold(Rational.ZERO){acc, curr -> acc + curr}
+            forAll(100, RationalGen()) { r ->
+                val egyptian = r.toEgyptianRational()
+                val sum = egyptian.fold(Rational.ZERO){acc, curr -> acc + curr}
                 r == sum
             }
         }
@@ -37,11 +36,25 @@ class EgyptianRationalRecursiveTest: StringSpec() {
 
 class EgyptianRationalImperativeTest: StringSpec() {
     init {
-        "recursive attempt" {
-            forAll(10, RationalGen()) { r ->
+        "imperative attempt" {
+            forAll(100, RationalGen()) { r ->
                 val egyptian = r.toEgyptianRationalImp()
                 val sum = egyptian.fold(Rational.ZERO){acc, curr -> acc + curr}
                 r == sum
+            }
+        }
+    }
+}
+
+class EgyptianRationalDirectTest: StringSpec() {
+    init {
+        "direct attempt" {
+            forAll(100, RationalGen()) { r ->
+                val egyptian = r.toEgyptianRationalDirect()
+                println(r)
+                println(egyptian)
+                val sum = egyptian?.fold(Rational.ZERO){acc, curr -> acc + curr}
+                sum == null || r == sum
             }
         }
     }
